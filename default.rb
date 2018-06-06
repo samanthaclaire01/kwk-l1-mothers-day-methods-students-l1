31f8e9300054e3b6371faf8037860b93f0655842
Samantha Cassity <cassityclaire@gmail.com>
2018-06-06 15:57:35 +0000

    Done.

---
+++ default.rb
@@ -0,0 +1,5 @@
+def car_rentals(type="Ford")
+  puts "Congratulations! You reserved a #{type}."
+end
+car_rentals("Mercedes")
+car_rentals()
---
+++ holiday_greeting.rb
@@ -0,0 +1,4 @@
+def holiday_greeting(recipient= "Mom!", your_name = "Your Favorite Child", holiday="Mother's Day")
+  puts "Happy #{holiday}, #{recipient}! from #{your_name}"
+end
+holiday_greeting("Beyonce", "Jay-Z", "Fourth of July")
